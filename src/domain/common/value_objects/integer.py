148e627628ca61ec31028e743f372308c206f72c
poorpen <roma.polovik.03@mail.ru>
2023-05-10 18:17:07 +0300

    Add IntegerInRenge value object

--- src/domain/common/value_objects/integer.py
+++ src/domain/common/value_objects/integer.py
@@ -1,13 +1,24 @@
 from dataclasses import dataclass
 
-from src.domain.common.value_objects.base import BaseVO
+from src.domain.common.value_objects.base import BaseValueObject
 
 
 @dataclass(frozen=True)
-class PositiveInteger(BaseVO[int]):
+class PositiveInteger(BaseValueObject[int]):
     min_value: int = 0
 
     @classmethod
-    def _validate(cls, v):
+    def _validate(cls, v: int) -> None:
         if v < cls.min_value:
             raise ValueError('value less than minimum value')
+
+
+@dataclass(frozen=True)
+class IntegerInRange(BaseValueObject[int]):
+    min_value: int = 0.0
+    max_value: int = 10.0
+
+    @classmethod
+    def _validate(cls, v: int) -> None:
+        if not cls.min_value <= v <= cls.max_value:
+            raise ValueError('value uot of range')
