4dace34b01b257f7ff742a9ca6bea17d5ceb2a21
poorpen <roma.polovik.03@mail.ru>
2023-05-15 14:50:52 +0300

    Extend functional for Text value objects

--- src/domain/common/value_objects/string.py
+++ src/domain/common/value_objects/string.py
@@ -4,20 +4,30 @@ from src.domain.common.value_objects.base import BaseValueObject
 
 
 @dataclass(frozen=True)
-class NonEmptyString(BaseValueObject[str]):
-
-    @classmethod
-    def _validate(cls, v: str) -> None:
-        if not v or v.isspace():
-            raise ValueError()
-
-
-@dataclass(frozen=True)
 class Text(BaseValueObject[str]):
-    max_len: int = 500
+    max_len: int
 
     @classmethod
     def _validate(cls, v: str) -> None:
         if len(v) > cls.max_len:
             raise ValueError(f'text lenght exceeds the allowed threshold of {cls.max_len} characters')
 
+
+@dataclass(frozen=True)
+class NonEmptyText(Text):
+    max_len: int = 500
+
+    @classmethod
+    def _validate_non_empty(cls, v: str) -> None:
+        if not v or v.isspace():
+            raise ValueError()
+
+    @classmethod
+    def _validate(cls, v: str) -> None:
+        cls._validate_non_empty(v)
+        super()._validate(v)
+
+
+@dataclass(frozen=True)
+class OptionalText(Text):
+    max_len: int = 500
