9a4622403c95011c23eca5d74b4a1077192d3584
poorpen <roma.polovik.03@mail.ru>
2023-07-09 20:43:20 +0300

    Add event for user

--- src/common/domain/models/aggregate.py
+++ src/common/domain/models/aggregate.py
@@ -1,5 +1,24 @@
 from abc import ABC
+from typing import List
+from dataclasses import dataclass, field
+
+from src.common.domain.models.event import Event
 
 
+@dataclass
 class AggregateRoot(ABC):
-    pass
+    _events: List[Event] = field(default_factory=list, init=False)
+
+    def _get_events(self) -> List[Event]:
+        return self._events.copy()
+
+    def _clear_events(self) -> None:
+        self._events.clear()
+
+    def record_event(self, event: Event) -> Event:
+        self._events.append(event)
+
+    def pull_events(self) -> List[Event]:
+        events = self._get_events()
+        self._clear_events()
+        return events
